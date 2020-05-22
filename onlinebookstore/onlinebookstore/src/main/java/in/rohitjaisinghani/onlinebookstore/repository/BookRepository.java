6de591abea722427b7be3f8e105fcaff8690e195
MAHESH <MAHESH@LAPTOP-FOI6HN55>
2020-05-22 16:11:37 +0530

    completed the development for feature 1

---
+++ onlinebookstore/onlinebookstore/src/main/java/in/rohitjaisinghani/onlinebookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package in.rohitjaisinghani.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.rohitjaisinghani.onlinebookstore.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+
+}
