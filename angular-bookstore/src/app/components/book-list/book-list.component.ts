5f353b2d64c6f8368477e9a1f68af196d90cc52b
MAHESH <MAHESH@LAPTOP-FOI6HN55>
2020-05-24 19:10:37 +0530

    completed feature 3 search by category

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,22 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { Book } from 'src/app/common/book';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
   books:Book[];
-  constructor(private _bookService:BookService) { }
+  currentCategoryId:number;
+
+  constructor(private _bookService:BookService,
+    private _activatedRoute:ActivatedRoute) { }
 
   //ngonit is a lifecycle method which will call as soon as BookListComponent is created
   ngOnInit() {
-    this.listBooks();
+    this._activatedRoute.paramMap.subscribe(()=>{
+      this.listBooks();
+    })
   }
   listBooks(){
-    this._bookService.getBooks().subscribe(
+
+    //this will return a boolean value which shows whether id is present or not
+    const hasCategoryId:boolean=this._activatedRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+
+      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+      //+  operator will convert string into integer
+    }
+    else{
+      this.currentCategoryId=1;
+    }
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
       data=>this.books=data
     )
   }
--- onlinebookstore/onlinebookstore/src/main/java/in/rohitjaisinghani/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/onlinebookstore/src/main/java/in/rohitjaisinghani/onlinebookstore/repository/BookRepository.java
@@ -1,6 +1,12 @@
 package in.rohitjaisinghani.onlinebookstore.repository;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.rohitjaisinghani.onlinebookstore.entity.Book;
@@ -9,5 +15,9 @@ import in.rohitjaisinghani.onlinebookstore.entity.Book;
 /*crossorigin will allow the host to access outside 8080
 if there are multiple host then * is used in round brackets*/
 public interface BookRepository extends JpaRepository<Book,Long> {
-
+	
+	
+	//to override the rest end points use RestResource
+	@RestResource(path="categoryid")
+	Page<Book> findByCategoryId(@Param("id")Long id,Pageable pageable);
 }
